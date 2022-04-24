b27c311e63473710b381d0aa3cb41230855ffd96
unknown <enriqueealog@gmail.com>
2022-04-24 14:12:51 -0500

    Adicion de vistas, modelos y controlador

--- WebApplication2/WebApplication2/Startup.cs
+++ WebApplication2/WebApplication2/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
 
 namespace WebApplication2
 {
@@ -24,6 +26,9 @@ namespace WebApplication2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<WebApplication2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebApplication2Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
